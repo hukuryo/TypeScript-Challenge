e9b419d55016042f35ed378553c554492b18c9c0
hukuryo <ryohei.1998.05.09@gmail.com>
2023-12-26 22:31:41 +0900

    Lesson1完了

---
+++ Lesson01/Lesson01.js
@@ -0,0 +1,5 @@
+var todo = {
+    title: "Hey",
+    description: "foobar",
+};
+console.log(todo);
--- sample/increment.ts
+++ sample/increment.ts
@@ -1,6 +1,6 @@
 class User {
     constructor( public name: string) {}
-  }
+}
   
-  const u = new User("taro");
-  console.log(u.name);
+const u = new User("Lesson");
+console.log(u.name);
