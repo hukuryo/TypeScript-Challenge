f05c64ccd4142d644f06f274086ae89dcc938901
hukuryo <ryohei.1998.05.09@gmail.com>
2023-12-27 23:04:57 +0900

    ファイルの場所を移動

--- Lesson01/Lesson01.ts
+++ Lesson01/Lesson01.ts
@@ -1,18 +1,18 @@
 // MyReadonly<T>を実装してください
 // MyReadonlyは、Tのプロパティをすべて読み取り専用にする型です
 type MyReadonly<T> = {
-    readonly [K in keyof T]: T[K];
-  }
-  
-  interface Todo {
-    title: string;
-    description: string;
-  }
-  
-  const todo: MyReadonly<Todo> = {
-    title: "Hey",
-    description: "foobar",
-  };
+  readonly [K in keyof T]: T[K];
+}
+
+interface Todo {
+  title: string;
+  description: string;
+}
+
+const todo: MyReadonly<Todo> = {
+  title: "Hey",
+  description: "foobar",
+};
   
   
 console.log(todo);
