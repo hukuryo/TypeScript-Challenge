57fe56fb7209111e397a3f3fc85b3cecf70da793
hukuryo <ryohei.1998.05.09@gmail.com>
2024-01-14 21:12:43 +0900

    コード修正

--- Lesson/Lesson10/lesson10.ts
+++ Lesson/Lesson10/lesson10.ts
@@ -3,7 +3,6 @@ type First<T extends any[]> = T extends [infer U, ...any[]] ? U : never;
 type arr1 = ["a", "b", "c"];
 type arr2 = [3, 2, 1];
 
-type head1 = First<arr1>; // expected to be 'a'
-type head2 = First<arr2>; // expected to be 3
-
+type head1 = First<arr1>;
+type head2 = First<arr2>;
 const arr: arr1 = ["a", "b", "c"];
