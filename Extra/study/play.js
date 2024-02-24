a46dd83c285bc9bfd248314661813245c8eb736d
hukuryo <ryohei.1998.05.09@gmail.com>
2024-02-24 09:07:44 +0900

    可変長引数の型定義

--- Extra/study/play.js
+++ Extra/study/play.js
@@ -14,3 +14,29 @@ var kota = {
     weight: 93
 };
 console.log(calcBMI(kota));
+var sums = function (base) {
+    var args = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        args[_i - 1] = arguments[_i];
+    }
+    var result = base * 100;
+    for (var _a = 0, args_1 = args; _a < args_1.length; _a++) {
+        var num = args_1[_a];
+        result += num;
+    }
+    return result;
+};
+console.log(1, 10, 100);
+console.log(123, 456);
+var toLowerOrUpper = function (str, upper) {
+    if (upper === void 0) { upper = false; }
+    if (upper) {
+        return str.toUpperCase();
+    }
+    else {
+        return str.toLowerCase();
+    }
+};
+console.log(toLowerOrUpper("Hello"));
+console.log((toLowerOrUpper("Hello", false)));
+console.log((toLowerOrUpper("Hello", true)));
--- Extra/study/play.ts
+++ Extra/study/play.ts
@@ -27,3 +27,27 @@ const kota: HumanCalc = {
 }
 
 console.log(calcBMI(kota))
+
+
+const sums = (base: number, ...args: number[]): number => {
+    let result = base * 100;
+    for (const num of args){
+        result += num;
+    }
+    return result;
+}
+
+console.log(1, 10, 100);
+console.log(123, 456);
+
+const toLowerOrUpper = (str: string, upper: boolean = false) => {
+    if(upper){
+        return str.toUpperCase();
+    }else{
+        return str.toLowerCase();
+    }
+}
+
+console.log(toLowerOrUpper("Hello"))
+console.log((toLowerOrUpper("Hello", false)))
+console.log((toLowerOrUpper("Hello", true)))
